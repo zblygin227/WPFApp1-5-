2bf35598f26b041f73f93dc49a0e451601533b97
исп-31 <-31@LAB17-05>
2022-10-27 13:42:57 +0300

    Добавьте файлы проекта.

---
+++ Lib_5/LibClass.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Lib_5
+{
+    public class LibClass
+    {
+        /// <summary>
+        /// Находит сумму целых чисел в диапозоне от -7 до 3
+        /// </summary>
+        /// <param name="genNums">Сгенерированные числа</param>
+        /// <param name="count">Кол-во сгенерированных чисел</param>
+        /// <param name="K">Предел суммы</param>
+        /// <returns>Сумму целых чисел</returns>
+        public static int SumOfNums(out string genNums, out int count, int K)
+        {
+            Random rnd = new Random();
+            int sum = 0;
+            genNums = "";
+            int randNum;
+            count = 0;
+            for (int i = 1; sum < K; i++)
+            {
+                randNum = rnd.Next(-7, 3);
+                genNums = $"{randNum} ; {genNums}";
+                if (randNum < 0) { randNum = rnd.Next(-7, 3); }
+                else
+                {
+                    sum += randNum;
+                    count = i;
+                }
+            }
+            return sum;
+        }
+
+        /// <summary>
+        /// Считает произведение чисел < 3
+        /// </summary>
+        /// <param name="arr">Одномерный массив</param>
+        /// <returns>Произведение чисел < 3</returns>
+        public static int PowOfNum(int[] arr)
+        {
+            int pow = 1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] < 3)
+                {
+                    pow *= arr[i];
+                }
+            }
+            return pow;
+        }
+        /// <summary>
+        /// Среднее
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static double AvgOfNumArr(int[,] arr)
+        {
+            double avg = 0;
+            for (int i = 1; i < arr.GetLength(1); i += 2)
+            {
+                avg = 0;
+                for (int j = 0; j < arr.GetLength(0); j++)
+                {
+                    avg += arr[i, j];
+                    avg /= arr.GetLength(0);
+                }
+            }
+            return avg;
+        }
+    }
+}
---
+++ WPFApp1(5)/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using Lib_5;
+
+namespace WPFApp1_5_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            NumK.Focus();
+        }
+
+        private void CloseProgram_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void taskShowed_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Разработчки: \nЗадание: Вычислить сумму целых случайных чисел, распределенных в диапазоне от -7 до 3, пока эта сумма не превышает некоторого числа K. Вывести на экран сгенерированные числа, значение суммы, и количество сгенерированных чисел.", "Номер вариант №8");
+        }
+
+        private void NumK_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            int.TryParse(NumK.Text, out int K);
+            SumOfNum.Text = $"{LibClass.SumOfNums(out string generNums, out int countGen, K)}";
+            GenNums.Text = generNums;
+            CountGenNums.Text = $"{countGen}";
+        }
+    }
+}
